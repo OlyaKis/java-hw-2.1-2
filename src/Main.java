67a308bab283a188a499eca5f307f5f0b3aab542
OlyaKis <haimashka@gmail.com>
2021-08-12 15:28:13 +0700

    New-2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int StartCount = 100;
+        int startCount = 100;
         int count = 1100;
         int refill = 1;
         int limit = 1000;
-            if (count >= limit) {
-                int BonusSum = (count / 100) * refill;
-                int sum = StartCount + count + BonusSum;
-                System.out.println(sum);
+        int bonusSum;
+            if (count > limit) {
+                bonusSum = (count / 100) * refill;
             } else {
-                int sum = StartCount + count;
-                System.out.println(sum);
+                bonusSum = 0;
         }
+        int sum = startCount + count + bonusSum;
+        System.out.println(sum);
     }
 }
